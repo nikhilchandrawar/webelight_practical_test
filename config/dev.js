9abf9af9badb5f94b2d0a47aaa96ff33526f6a7d
nikhilchandrawar <89628390+nikhilchandrawar@users.noreply.github.com>
2022-08-14 18:17:10 +0530

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 module.exports = {
-  mongoURI: `mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb:${process.env.27017}/${process.env.webelight}`,
+  mongoURI: `mongodb://${process.env.ip-192-168-0-11.ap-south-1.compute.internal}:${process.env.27017}/${process.env.webelight}`,
   redisPort: process.env.8001,
   redisHost: process.env.ip-192-168-0-11.ap-south-1.compute.internal
 };
